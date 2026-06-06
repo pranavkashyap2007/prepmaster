591b964afba27e1ae2089aca42b81d4b8af377bf
pranavkashyap2007 <89726096+pranavkashyap2007@users.noreply.github.com>
2026-06-06 21:00:27 +0530

    Delete assets directory

--- assets/chevron-down-BSxCt6_i.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./index-C7rs4zCg.js";var t=e(`chevron-down`,[[`path`,{d:`m6 9 6 6 6-6`,key:`qrunsl`}]]);export{t};
--- assets/chevron-right-Lp9v2eRG.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./index-C7rs4zCg.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
